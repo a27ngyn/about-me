c3b0cf41a6113b5afd3636b5405ee99cd3ba59ad
Aurelia Nguyen <aurelia.nguyen27@gmail.com>
2023-07-17 15:25:44 -0700

    finish project

--- about me/ContentView.swift
+++ about me/ContentView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textTitle = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            VStack {
+                Text("Hi! I'm Aurelia!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+               
+                Image("IMG_4712")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                Button("Click Here!") {
+                    textTitle = "Here are some facts about me!!!                   I am from McLean, Virginia. I have an older sister in college. I have 3 dogs named, Miso, Mochi, and Ash!"
+                    }
+                Text(textTitle)
+                }
+                
+                    
+                
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .cornerRadius(50)
+            .shadow(radius: 50)
+            .padding()
         }
-        .padding()
+   
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
